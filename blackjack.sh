c0b80529b15ed452dc8eb5f1fa2ea0b4b500936f
Isaac Lins <isaac.lins07@gmail.com>
2024-06-28 11:32:15 +0200

    chore: Update blackjack.sh script with rules option and adds second card read

--- blackjack.sh
+++ blackjack.sh
@@ -14,39 +14,49 @@ fi
 # Check if help flag is provided
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Usage: blackjack.sh [OPTIONS]"
+    echo "----------------------------------------------------------------------------"
     echo "Options:"
-    echo "  --help, -h     Display this help message"
-    echo "  --win,  -w     Helps you play the game with the best winrate possible"
-    echo "  --play, -p     Play a game of blackjack"
-    echo "------------------------------------------"
-    echo "Explanation:"
-    echo 
-    echo "SOFT HAND: A hand containing an ace valued as 11 is called a SOFT hand."
-    echo "HARD HAND: A hand without an ace or with an ace valued as 1 is called a HARD hand."
-
-    echo "Cards from 2-10 are worth their face value. Face cards (Jack, Queen, King) are worth 10. Aces are worth 1 or 11."
-    echo "you add the Cards together and that is equal to your HAND."
-    echo "The goal of the game is to have a HAND higher than the Dealers without exceeding 21 from both parties. "
-    echo "If you exceed 21, you BUST and lose the game. If the Dealer exceeds 21, you WIN the game."
-    echo "If you and the Dealer have the same HAND, it is a PUSH and no one wins."
-    echo "If you have a HAND of 21, you have a BLACKJACK and win the game."
-    echo "If you have a HAND of 21 and the Dealer has a HAND of 21, it is a PUSH."
-    echo "If you have a HAND of higher the one of the Dealer, you win the game."
-    echo ""
+    echo "  --help,     -h      Display this help message"
+    echo "  --win,      -w      Helps you play the game with the best winrate possible"
+    echo "  --play,     -p      Play a game of blackjack"
+    echo "  --rules,    -r      Explains the rules of the game"
+    echo "----------------------------------------------------------------------------"
     exit 0
 fi
 
+if [ "$1" = "--rules" ] || [ "$1" = "-r" ]; then
+    echo "Rules:"
+    echo "----------------------------------------------------------------------------"
+    echo "1. Cards from 2-10 are worth their face value. Face cards (Jack, Queen, King) are worth 10. Aces are worth 1 or 11."
+    echo "2. You add the Cards together and that is equal to your HAND."
+    echo "3. If you have a HAND of higher the one of the Dealer, you win the game."
+    echo "4. If you exceed 21, you BUST and lose the game."
+    echo "5. If the Dealer exceeds 21, you WIN the game."
+    echo "----------------------------------------------------------------------------"
+fi
 if [ "$1" = "--win" ] || [ "$1" = "-w" ]; then
         echo "Please enter your FIRST CARD:"
         echo "1-10 or J, Q, K, A"
-        while [ "$input" != "e" ] || [ "$input" != "E" ]; do
+        while [ "$input" != "exit" ] || [ "$input" != "e" ]; do
         
-        read input
-        if [[ "$input" =~ ^[1-9]$|^10$|^J$|^Q$|^K$|^A$ ]]; then
-            echo "Your input: $input"
-        else
-            echo "Invalid input. Please enter a number from 1-10 or J, Q, K, A."
-        fi
-        
+            read input
+            if [[ "$input" =~ ^[1-9]$|^10$|^J$|^Q$|^K$|^A$|^j$|^q$|^k$|^a$ ]]; then
+                echo "Your input: $input"
+                hand+=("$input")
+            else
+                echo "Invalid input."
+                echo "Please enter a number from 1-10 or J, Q, K, A."
+                continue
+            fi
+            hand=("$input")
+            echo "Please enter your SECOND CARD:"
+                read input
+                if [[ "$input" =~ ^[1-9]$|^10$|^J$|^Q$|^K$|^A$|^j$|^q$|^k$|^a$ ]]; then
+                    echo "Your input: $input"
+                    hand+=("$input")  
+                else
+                    echo "Invalid input. Please enter a number from 1-10 or J, Q, K, A."
+                fi
+            echo "Your hand: ${hand[@]}"
         done
 fi
