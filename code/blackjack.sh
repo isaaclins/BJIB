96a85e6bb34d45dfed54075a18107448db20573e
isaaclins <isaac.lins07@gmail.com>
2024-07-08 14:06:20 +0200

    added GUI :3

--- code/blackjack.sh
+++ code/blackjack.sh
@@ -13,6 +13,31 @@
 # Author: Isaaclins <3
 # Version: 1.0
 
+topcharacter() {
+    echo -e "|$1      |"
+}
+
+bottomcharacter() {
+    echo -e "|      $1|"
+}
+
+print_characters() {
+    local top=' ------- '
+    local side='|       |'
+    local topout=''
+    local topcharacter=''
+    local bottomcharacter=''
+    local blankside=''
+    for character in "$@"
+    do
+        topout+="$top "
+        topcharacter+="$(topcharacter $character) "
+        blankside+="$side "
+        bottomcharacter+="$(bottomcharacter $character) "
+    done
+    echo -e "$topout\n$topcharacter\n$blankside\n$blankside\n$blankside\n$bottomcharacter\n$topout"
+}
+
 
 playgame(){
     player_score=0
